ed3950870f43c5df97cbb186baf69ed26c167839
Nazmul Hoque <sujon686500@gmail.com>
2024-05-26 12:56:40 +0600

    refactoring card details

--- src/Overview.jsx
+++ src/Overview.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Grid, Box, Typography } from "@mui/material";
+import { Grid, Box, Typography, Alert, AlertTitle } from "@mui/material";
 import CardDetails from "./CardDetails";
 
 Overview.propTypes = {
@@ -29,13 +29,19 @@ export default function Overview({ projects }) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       {projects.map((project, index) => (
-        <Grid key={index} container spacing={2} mb={2}>
-          {project.urls.map((urlData, index) => (
-            <Grid item xs={12} sm={6} md={4} key={index}>
-              <CardDetails urlData={urlData} />
-            </Grid>
-          ))}
-        </Grid>
+        <React.Fragment>
+          <Alert severity="success" sx={{ mb: 2, textTransform: "capitalize" }}>
+            <AlertTitle>{project.name}</AlertTitle>
+            This is a success Alert with an encouraging title.
+          </Alert>
+          <Grid key={index} container spacing={2} mb={2}>
+            {project.urls.map((urlData, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <CardDetails urlData={urlData} />
+              </Grid>
+            ))}
+          </Grid>
+        </React.Fragment>
       ))}
     </Box>
   );
